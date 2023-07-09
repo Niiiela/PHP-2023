bf430017a6f2b2a72e07e7ed63582a7f48e3ecb2
niiiela <danielavieira2024@gmail.com>
2023-07-09 10:08:42 -0300

    Operadores Aritméticos

---
+++ _Operadores Aritméticos/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="css/style.css"/>
+        <title>Operadores Aritméticos</title>
+    </head>
+    <body>
+    <div class="ex0">
+            <h1>Aprendendo PHP</h1>
+            <h2>Variáveis em <span>PHP</span></h2>
+        </div>
+        <div class="ex1">
+            <?php
+            /*
+            Operadores Aritméticos
+
+            Adição: $a + $b
+            Subtração: $c - $d
+            Multiplicação: $b * $a
+            Divisão: $c / $a
+            Módulo: $e % $f
+            */
+
+                $n1= 3;
+                $n2= 2;
+                $s = $n1 + $n2;
+                echo "<br><br><br> A soma entre $n1 e $n2 e igual a $s";
+            ?>
+        </div>
+    </body>
+</html>
