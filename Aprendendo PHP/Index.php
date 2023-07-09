a18999650bf356d63cd08e8a9513fbb92e41b820
niiiela <danielavieira2024@gmail.com>
2023-07-09 09:30:41 -0300

    Aprendendo PHP

---
+++ Aprendendo PHP/Index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Primeiro exemplo PHP</title>
+        <style>
+            h2
+            {
+                color: cadetblue;
+                text-shadow: 1px 1px 1px black;
+            }
+        </style>
+    </head>
+
+    <body>
+        <h1>Testando Php</h1>
+
+        <?php
+            echo '<h2>Olá, <br/>mundo!</h2>'; 
+        ?>
+    </body>
+</html>
